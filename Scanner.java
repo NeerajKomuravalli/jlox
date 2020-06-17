23549213bdf290e5d5f4de3d7e13076e25076421
Neeraj Komuravalli <komunee2710@gmail.com>
2020-06-18 02:29:46 +0530

    Adding Scanner.java (Fully implemented except reserved words and identifiers)

---
+++ Scanner.java
@@ -0,0 +1,133 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Scanner{
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 0;
+
+    Scanner(String source){
+        this.source = source;
+    }
+    List<com.craftinginterpreters.lox.Token> scanTokens(){
+        while (!isAtEnd()){
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+    private void scanToken(){
+        char c = advance();
+        switch (c){
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+            case '/':
+                if (match('/')){
+                    //This means we have reached a comment and we need to read it untill we reach the end ('\n')
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace.
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if (isDigit(c)){
+                    number();
+                }else {
+                    com.craftinginterpreters.lox.Lox.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+    private void number(){
+        while (isDigit(peek())) advance();
+
+        // Handling floating point
+        if (peek() == '.' && isDigit(peekNext())){
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER,
+                Double.parseDouble(source.substring(start, current)));
+    }
+    private void string(){
+        while (peek() != '"' && !isAtEnd()){
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        // Edge case
+        if (isAtEnd()){
+            com.craftinginterpreters.lox.Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // For the trailing '"'
+        advance();
+
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
+    }
+    private boolean match(char expected){
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current ++;
+        return true;
+    }
+    private char peek(){
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+    private char peekNext(){
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+    private char advance(){
+        current ++;
+        return source.charAt(current - 1);
+    }
+    private void addToken(com.craftinginterpreters.lox.TokenType type){
+        addToken(type, null);
+    }
+    private void addToken(com.craftinginterpreters.lox.TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line))
+    }
+}
