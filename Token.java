b67265ea4b3f972fbe5df1090ba7d863973ef335
Neeraj Komuravalli <komunee2710@gmail.com>
2020-06-18 02:28:34 +0530

    Adding the complete import names

--- Token.java
+++ Token.java
@@ -1,12 +1,12 @@
 package com.craftinginterpreters.lox;
 
 class Token {
-  final TokenType type;
+  final com.craftinginterpreters.lox.TokenType type;
   final String lexeme;
   final Object literal;
   final int line; 
 
-  Token(TokenType type, String lexeme, Object literal, int line) {
+  Token(com.craftinginterpreters.lox.TokenType type, String lexeme, Object literal, int line) {
     this.type = type;
     this.lexeme = lexeme;
     this.literal = literal;
