c9bdc161e3a916ac1f90e5b38086aff8e0c64d04
Neeraj Komuravalli <komunee2710@gmail.com>
2020-09-14 13:08:30 +0530

    Adding the last part of class intergartion :: inheritence (did not properly test it)

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -23,7 +23,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     private ClassType currentClass = ClassType.NONE;
@@ -184,6 +185,19 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword,
+                    "Cannot use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword,
+                    "Cannot use 'super' in a class with no superclass.");
+        }
+        resolveLocal(expr, expr.keyword);
+        return null;
+    }
+
+    @Override
     public Void visitThisExpr(Expr.This expr) {
         if (currentClass == ClassType.NONE) {
             Lox.error(expr.keyword,
@@ -245,6 +259,21 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         declare(stmt.name);
         define(stmt.name);
+        if (stmt.superclass != null
+                && stmt.name.lexeme.equals(stmt.superclass.name.lexeme)){
+            // This is to avoid a case like : "class Oops < Oops {}"
+            Lox.error(stmt.superclass.name,
+                    "A class cannot inherit from itself.");
+        }
+        if (stmt.superclass != null){
+            currentClass = ClassType.SUBCLASS;
+            resolve(stmt.superclass);
+        }
+
+        if (stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
+        }
 
         beginScope();
         scopes.peek().put("this", true);
@@ -257,6 +286,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             resolveFunction(method, declaration);
         }
         endScope();
+
+        if (stmt.superclass != null) endScope();
 
         currentClass = enclosingClass;
         return null;
--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -23,6 +23,7 @@ public class GenerateAst{
                     "Call     : Expr callee, Token paren, List<Expr> arguments",
                     "Get : Expr object, Token name",
                     "Set      : Expr object, Token name, Expr value",
+                    "Super    : Token keyword, Token method",
                     "This     : Token keyword"
             ));
             defineAst(outputDir, "Stmt", Arrays.asList(
@@ -34,7 +35,7 @@ public class GenerateAst{
                     "Print : Expr expression",
                     "Var : Token name, Expr initializer",
                     "Return : Token keyword, Expr value",
-                    "Class : Token name, List<Stmt.Function> methods"
+                    "Class : Token name, Expr.Variable superclass, List<Stmt.Function> methods"
             ));
         }
     }
